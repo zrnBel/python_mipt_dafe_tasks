3a853abc83501dbb0fc3c3fb04cd9c6f99af3429
Kirill Beloborodov <beloborodovkirill-07@mail.ru>
2025-10-31 11:03:02 +0300

    task lesson6

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,14 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    seen = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(text)):
+        while text[right] in seen:
+            seen.remove(text[left])
+            left += 1
+
+        seen.add(text[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
--- solutions/lesson06/task4.py
+++ solutions/lesson06/task4.py
@@ -1,3 +1,17 @@
 def count_unique_words(text: str) -> int:
-    # ваш код
-    return 0
+    unique_words = set()
+    buffer = ""
+
+    for char in text:
+        if char.isalnum() or char == "'":
+            buffer += char.upper()
+            continue
+
+        if buffer:
+            unique_words.add(buffer)
+            buffer = ""
+
+    if buffer:
+        unique_words.add(buffer)
+
+    return len(unique_words)
