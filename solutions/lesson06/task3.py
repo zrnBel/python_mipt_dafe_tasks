92668cd749782ec5a2739a903c898d981ee56058
Kirill Beloborodov <beloborodovkirill-07@mail.ru>
2025-10-31 11:17:19 +0300

    New lesson6

--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -2,6 +2,8 @@ def is_there_any_good_subarray(
     nums: list[int],
     k: int,
 ) -> bool:
-    
-    # ваш код
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if sum(nums[i : j + 1]) % k == 0:
+                return True
     return False
