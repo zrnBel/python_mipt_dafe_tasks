86da13048f48a2f01dfb0f75519be6a17bc06264
Kirill Beloborodov <beloborodovkirill-07@mail.ru>
2025-11-09 17:01:22 +0300

    Task lesson8

---
+++ solutions/lesson08/task2.py
@@ -0,0 +1,28 @@
+from typing import Callable, TypeVar, ParamSpec
+from functools import wraps
+from time import time
+
+T = TypeVar("T")
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def collect_statistic(statistics: dict[str, list[float, int]]) -> Callable[[T], T]:
+    def collect_function(func: Callable[P, R]) -> Callable[P, R]:
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            start = time()
+            result = func(*args, **kwargs)
+            duration = time() - start
+
+            avg, count = statistics.get(func.__name__, [0.0, 0])
+
+            count += 1
+            avg = avg + (duration - avg) / count
+
+            statistics[func.__name__] = [avg, count]
+            return result
+
+        return wrapper
+
+    return collect_function
