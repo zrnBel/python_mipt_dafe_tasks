2fda98672f0247da9591b7cf10f78451b10e5d57
lysenkov.evg@gmail.com <lysenkov.evg@gmail.com>
2025-11-16 23:33:29 +0300

    Add tasks for hw1

---
+++ homeworks/hw1/convert_exception.py
@@ -0,0 +1,28 @@
+from typing import (
+    Callable,
+    ParamSpec,
+    TypeVar,
+)
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def convert_exceptions_to_api_compitable_ones(
+    exception_to_api_exception: dict[type[Exception], type[Exception] | Exception],
+) -> Callable[[Callable[P, R]], Callable[P, R]]:
+    """
+    Параметризованный декоратор для замены внутренних исключений на API-исключении.
+
+    Args:
+        exception_to_api_exception: словарь:
+            ключи - внутренние исключения, которые надо заменить,
+            значения - API-исключения, которые надо возбудить
+                вместо внутренних исключений
+
+    Returns:
+        Декоратор для непосредственного использования.
+    """
+
+    # ваш код
+    pass
