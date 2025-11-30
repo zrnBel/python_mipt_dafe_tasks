e54b0615dac9d71d2a4215da1a3e22b618a518e8
Kirill Beloborodov <beloborodovkirill-07@mail.ru>
2025-11-30 15:15:36 +0300

    fix

--- homeworks/hw1/cache.py
+++ homeworks/hw1/cache.py
@@ -6,20 +6,21 @@ from typing import (
     TypeVar,
 )
 
+
 class func_args:
     args: tuple
     kwargs: tuple
 
-    def __init__(self,args: tuple, kwargs: dict) -> None:
+    def __init__(self, args: tuple, kwargs: dict) -> None:
         self.args = args
         self.kwargs = tuple(sorted(kwargs.items()))
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, func_args):
             return NotImplemented
-        
+
         return (self.args, self.kwargs) == (other.args, other.kwargs)
-        
+
     def __hash__(self) -> int:
         return hash((self.args, self.kwargs))
 
@@ -43,19 +44,19 @@ def lru_cache(capacity: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
             для получения целого числа.
         ValueError, если после округления capacity - число, меньшее 1.
     """
-    
+
     try:
         capacity = int(round(capacity))
     except Exception:
         raise TypeError
-    
+
     if capacity < 1:
         raise ValueError
 
-    def add_cache(func: Callable[P, R]) -> Callable[P,R]:
-        cache: OrderedDict = OrderedDict() 
+    def add_cache(func: Callable[P, R]) -> Callable[P, R]:
+        cache: OrderedDict = OrderedDict()
 
-        @wraps(func)      
+        @wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
             nonlocal cache
             key = func_args(args, kwargs)
@@ -63,7 +64,7 @@ def lru_cache(capacity: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
             if key in cache:
                 cache.move_to_end(key)
                 return cache[key]
-            
+
             else:
                 result = func(*args, **kwargs)
                 cache[key] = result
@@ -72,7 +73,7 @@ def lru_cache(capacity: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
                     cache.popitem(last=False)
 
                 return result
-            
+
         return wrapper
-    
+
     return add_cache
