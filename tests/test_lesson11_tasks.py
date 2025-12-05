69eae5e3bd656227e490e840f055089550ec54fd
lysenkov.evg@gmail.com <lysenkov.evg@gmail.com>
2025-12-05 18:17:57 +0300

    Fix print test for les11

--- tests/test_lesson11_tasks.py
+++ tests/test_lesson11_tasks.py
@@ -83,9 +83,12 @@ def test_print():
 
     sys.stdout = old_stdout
     output = captured_output.getvalue()
-    assert (output == "Vector2D(abscissa=1, ordinate=-2)") or (
-        output == "Vector2D(abscissa=1., ordinate=-2.)"
+    assert (
+        output == "Vector2D(abscissa=1, ordinate=-2)" 
+        or output == "Vector2D(abscissa=1., ordinate=-2.)"
+        or output == "Vector2D(abscissa=1.0, ordinate=-2.0)" 
     )
+
 
 
 @pytest.mark.parametrize(
