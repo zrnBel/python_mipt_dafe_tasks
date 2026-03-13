198a8734d6299ebb6d7078536bde8441ca8b8b88
zrnBel <beloborodovkirill-07@mail.ru>
2026-03-13 21:04:48 +0300

    task lesson04

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,49 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError("Pad size must be greater then 1")
+
+    if image.ndim == 3:
+        h, w, c = image.shape
+        result_image = np.zeros((h + 2 * pad_size, w + 2 * pad_size, c), dtype=np.uint8)
+        result_image[pad_size:-pad_size, pad_size:-pad_size, :] = image
+
+    else:
+        h, w = image.shape
+        result_image = np.zeros((h + 2 * pad_size, w + 2 * pad_size), dtype=np.uint8)
+        result_image[pad_size:-pad_size, pad_size:-pad_size] = image
+
+    return result_image
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size < 1:
+        raise ValueError("Kernel size must pe greater then 1")
+
+    if kernel_size == 1:
+        return image
+
+    if kernel_size % 2 == 0:
+        raise ValueError("Kernel value must be odd")
+
+    pad_size = kernel_size // 2
+    image = pad_image(image, pad_size) / kernel_size**2
+
+    np.cumsum(image, axis=1, out=image)
+    values_horizontal_offset = np.zeros(shape=image.shape)
+    values_horizontal_offset[::, kernel_size::] = image[::, :-kernel_size]
+    image -= values_horizontal_offset
+
+    np.cumsum(image, axis=0, out=image)
+    values_vertical_offset = np.zeros(shape=image.shape)
+    values_vertical_offset[kernel_size::, ::] = image[:-kernel_size, ::]
+    image -= values_vertical_offset
+
+    return np.array(image[pad_size * 2 : :, pad_size * 2 : :], dtype=np.uint8)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,22 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    unique_colors, unique_colors_count = np.unique(image, return_counts=True)
+    colors_count = np.zeros(256)
+    colors_count[unique_colors] = unique_colors_count
+
+    pixels_in_dominant_group = 0
+    for color in unique_colors:
+        left_color_border = max(0, int(color) - threshold + 1)
+        right_color_border = min(255, int(color) + threshold - 1)
+
+        pixels_in_current_group = np.sum(colors_count[left_color_border : right_color_border + 1])
+
+        if pixels_in_current_group > pixels_in_dominant_group:
+            pixels_in_dominant_group = pixels_in_current_group
+            dominant_color = color
+
+    return dominant_color, float(pixels_in_dominant_group / image.size * 100)
