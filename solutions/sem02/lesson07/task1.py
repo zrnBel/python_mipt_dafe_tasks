4d26b700d2e24af730ea3f4e5a41369c2d7f569a
zrnBel <beloborodovkirill-07@mail.ru>
2026-04-10 20:55:14 +0300

    task lesson07

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -1,7 +1,39 @@
-from typing import Any
+from typing import Any, Final
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+ALLOWED_DIAGRAM_TYPES: Final = frozenset(
+    [
+        "hist",
+        "box",
+        "violin",
+    ]
+)
+
+
+class ColorConfig:
+    scatter: Final[dict] = {
+        "color": "#7FA6B5",
+        "edgecolors": "#647075",
+        "alpha": 0.6,
+    }
+
+    hist: Final[dict] = {
+        "color": "#BFAB95",
+        "edgecolor": "#4B4033",
+        "alpha": 0.7,
+    }
+
+    box: Final[dict] = {
+        "boxprops": {"facecolor": "#BFAB95", "edgecolor": "#4B4033", "alpha": 0.8},
+        "medianprops": {
+            "color": "#4B4033",
+        },
+    }
+
+    violin: Final[str] = "#BFAB95"
+    violin_edge: Final[str] = "#4B4033"
 
 
 class ShapeMismatchError(Exception):
@@ -13,8 +45,52 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if diagram_type not in ALLOWED_DIAGRAM_TYPES:
+        raise ValueError
+
+    if abscissa.size != ordinates.size:
+        raise ShapeMismatchError
+
+    figure = plt.figure(figsize=(16, 9))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    scatter = figure.add_subplot(grid[:-1, 1:])
+    vert_axis = figure.add_subplot(grid[:-1, 0], sharey=scatter)
+    hor_axis = figure.add_subplot(grid[-1, 1:], sharex=scatter)
+
+    scatter.scatter(abscissa, ordinates, s=70, **ColorConfig.scatter)
+    match diagram_type:
+        case "hist":
+            vert_axis.hist(
+                ordinates,
+                bins=70,
+                orientation="horizontal",
+                **ColorConfig.hist,
+            )
+            hor_axis.hist(
+                abscissa,
+                bins=70,
+                **ColorConfig.hist,
+            )
+
+        case "box":
+            vert_axis.boxplot(ordinates, vert=True, **ColorConfig.box)
+            hor_axis.boxplot(abscissa, vert=False, **ColorConfig.box)
+
+        case "violin":
+            vert_violin = vert_axis.violinplot(ordinates, showmedians=True, vert=True)
+            hor_violin = hor_axis.violinplot(abscissa, showmedians=True, vert=False)
+
+            for body in vert_violin["bodies"]:
+                body.set_facecolor(ColorConfig.violin)
+                body.set_edgecolor(ColorConfig.violin_edge)
+
+            for body in hor_violin["bodies"]:
+                body.set_facecolor(ColorConfig.violin)
+                body.set_edgecolor(ColorConfig.violin_edge)
+
+    hor_axis.invert_yaxis()
+    vert_axis.invert_xaxis()
 
 
 if __name__ == "__main__":
@@ -24,5 +100,5 @@ if __name__ == "__main__":
 
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
 
-    visualize_diagrams(abscissa, ordinates, "hist")
+    visualize_diagrams(abscissa, ordinates, "violin")
     plt.show()
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,94 @@
 # ваш код (используйте функции или классы для решения данной задачи)
+import json
+from typing import Dict, Final, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use("seaborn-v0_8")
+
+arabic_to_roman: Final[Dict[int, str]] = {
+    1: "I",
+    2: "II",
+    3: "III",
+    4: "IV",
+}
+
+
+class PlotConfig:
+    STAGES_AMOUNT: Final[int] = 4
+    BAR_WIDTH: Final[float] = 0.35
+
+    class Colors:
+        before: Final[str] = "#7FA6B5"
+        after: Final[str] = "#BFAB95"
+        before_edge: Final[str] = "#647075"
+        after_edge: Final[str] = "#4B4033"
+
+    class Fonts:
+        labels: Final[dict] = {"fontsize": 17, "fontweight": "bold", "color": "dimgray"}
+        titles: Final[dict] = {"fontsize": 23, "fontweight": "bold", "color": "dimgray"}
+        legend: Final[dict] = {
+            "fontsize": 20,
+        }
+
+
+def load_data(path_to_file: str) -> Tuple[np.ndarray, np.ndarray]:
+    patients_stages_before = np.zeros(PlotConfig.STAGES_AMOUNT, dtype=np.uint64)
+    patients_stages_after = np.zeros(PlotConfig.STAGES_AMOUNT, dtype=np.uint64)
+
+    with open(path_to_file) as file:
+        data = json.load(file)
+
+        for degree in range(PlotConfig.STAGES_AMOUNT):
+            patients_stages_before[degree] = data["before"].count(arabic_to_roman[degree + 1])
+            patients_stages_after[degree] = data["after"].count(arabic_to_roman[degree + 1])
+
+    return patients_stages_before, patients_stages_after
+
+
+def create_diagram(path_to_file: str, file_name: str, width: int, height: int, dpi: int) -> None:
+    if not path_to_file or not file_name:
+        raise ValueError
+
+    if width < 0 or height < 0 or dpi < 0:
+        raise ValueError
+
+    patients_stages_before, patients_stages_after = load_data(path_to_file)
+
+    x_ticks = np.arange(PlotConfig.STAGES_AMOUNT)
+
+    figure, axis = plt.subplots(figsize=(width, height))
+
+    axis.bar(
+        np.arange(patients_stages_before.size) - PlotConfig.BAR_WIDTH / 2,
+        patients_stages_before,
+        label="before",
+        width=PlotConfig.BAR_WIDTH,
+        color=PlotConfig.Colors.before,
+        edgecolor=PlotConfig.Colors.before_edge,
+    )
+    axis.bar(
+        np.arange(patients_stages_after.size) + PlotConfig.BAR_WIDTH / 2,
+        patients_stages_after,
+        label="after",
+        width=PlotConfig.BAR_WIDTH,
+        color=PlotConfig.Colors.after,
+        edgecolor=PlotConfig.Colors.after_edge,
+    )
+
+    axis.set_title("Mitral disease stages", **PlotConfig.Fonts.titles)
+    axis.set_ylabel("amount of people", **PlotConfig.Fonts.labels)
+
+    axis.set_xticks(x_ticks)
+    axis.set_xticklabels(
+        [arabic_to_roman[i + 1] for i in range(PlotConfig.STAGES_AMOUNT)], **PlotConfig.Fonts.labels
+    )
+
+    axis.legend(**PlotConfig.Fonts.legend)
+
+    figure.savefig(file_name, bbox_inches="tight", dpi=dpi)
+
+
+if __name__ == "__main__":
+    create_diagram("data/medic_data.json", "medic_data_diag", 16, 9, 300)
